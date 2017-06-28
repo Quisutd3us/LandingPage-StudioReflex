6b57b6839bdddac9052334c5647b1b4d78d89db2
David Salinas <dnarino@gmail.com>
2017-06-27 22:29:01 -0500

    Animacion
    Manu sticky + smooth desplazamiento

--- js/misScripts.js
+++ js/misScripts.js
@@ -1,2 +1,26 @@
 /* Iniciamos WOW*/
     new WOW().init();
+
+
+/* Iniciamos Smooth Scroll*/
+    smoothScroll.init({
+    	speed:1000,
+    	offset:60,
+    });
+
+/* Iniciamos Efecto al NavBar*/
+
+$(window).scroll(function(){
+
+	var nav = $('.david');
+	var scroll = $(window).scrollTop();
+	if(scroll >= 600){
+		nav.addClass('claseDavid');
+		console.log('llego');
+	} 
+	else{
+		nav.removeClass('claseDavid');
+	}
+});
+
+
